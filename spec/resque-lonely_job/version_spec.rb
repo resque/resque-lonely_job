bac7460ad148580b94b77530d66cae53d119d235
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 17:48:33 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/resque-lonely_job/version_spec.rb
+++ spec/resque-lonely_job/version_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "anonymous_loader"
+require "resque-lonely_job"
 RSpec.describe Resque::Plugins::LonelyJob::Version do
   it_behaves_like "a Version module", described_class
 
